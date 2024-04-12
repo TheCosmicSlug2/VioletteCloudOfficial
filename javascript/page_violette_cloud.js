c12e5d6be08cf27ee3969d58b78e4c04b9fa8b73
TheCosmicSlug2 <159659123+TheCosmicSlug2@users.noreply.github.com>
2024-04-12 21:21:27 +0200

    Add files via upload

---
+++ javascript/page_violette_cloud.js
@@ -0,0 +1,24 @@
+function fete_musique() {
+    document.getElementById("image_concerts").src = "images/fete_musique.png";
+}
+function trois_fils() {
+    document.getElementById("image_concerts").src = "images/trois_fils.jpg";
+}
+function vino_durum() {
+    document.getElementById("image_concerts").src = "images/vino.jpg";
+}
+function brocante() {
+    document.getElementById("image_concerts").src = "images/brocante.jpg";
+}
+function rugby1() {
+    document.getElementById("image_concerts").src = "images/rugby1.jpg";
+}
+function rugby2() {
+    document.getElementById("image_concerts").src = "images/rugby2.png";
+}
+function kinepolis() {
+    document.getElementById("image_concerts").src = "images/zzzzzzzzzzzzzzzzzzzzzzzzzzzzzz";
+}
+function rue() {
+    document.getElementById("image_concerts").src = "images/rue.jpg";
+}
