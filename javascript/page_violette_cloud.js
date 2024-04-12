a3e8e2990d77185bffba7d3f7f2fb3b8b07fb18b
TheCosmicSlug2 <159659123+TheCosmicSlug2@users.noreply.github.com>
2024-04-12 22:43:24 +0200

    Update page_violette_cloud.js

--- javascript/page_violette_cloud.js
+++ javascript/page_violette_cloud.js
@@ -1,24 +1,24 @@
 function fete_musique() {
-    document.getElementById("image_concerts").src = "images/fete_musique.png";
+    document.getElementById("image_concerts").src = "https://lh3.googleusercontent.com/pw/AP1GczPG59zKDn78URUw3dPPSObn3neKKnMlkY7vnBphVMVbaUBkEmfGL0jwHd_i9Cz2mJdlpG_kDmQ935UqyWmgOqOKVTvUDuiTT9h4JVp0kXJQ1425-b9zVqbEJsWXdqzSaCfOMlJ15du49x7kL4FP0UYJ";
 }
 function trois_fils() {
-    document.getElementById("image_concerts").src = "images/trois_fils.jpg";
+    document.getElementById("image_concerts").src = "https://lh3.googleusercontent.com/pw/AP1GczOHnOHYWkt2C8snIROXkIeYMRYNf5_2bUvkCI-1ruXj7GyWit0cFcc1jIyojBwoHnxJ1WfeiHpDuflpTN3lM38GZK0yC1GaErtrDwchFXgoiib2BYohWNIaQM14wJq6G1jc6Yo5tuqBMQ_kkncgabZx";
 }
 function vino_durum() {
-    document.getElementById("image_concerts").src = "images/vino.jpg";
+    document.getElementById("image_concerts").src = "https://lh3.googleusercontent.com/pw/AP1GczPneTMxQsJftsgda5KhvwOM42NIrQuQP3BFaG7whA84jwVQXYs33uzoGxICSMvXe9kN_RODXAmowuuTG1RdKFBuT5cwN_fY1MGq5zLaaYuoHlHDg0bY6FvTGeLydG3Gs0N-7OHnniMbhHYo-3BYne7D";
 }
 function brocante() {
-    document.getElementById("image_concerts").src = "images/brocante.jpg";
+    document.getElementById("image_concerts").src = "https://lh3.googleusercontent.com/pw/AP1GczOjgRnT0kdH8f03RFoO3xCxPdHuDKeYQDBh8_7ckre2tEdqUcdKPCvW723yefkyLj7cCl0tNXWUY7oLOELCR4bdTblfW-mtTOH9QfXk6Bsjnw_71BQiKL2Fv794IAbuqH23p-sJ81PruSKGq1pYKuRR";
 }
 function rugby1() {
-    document.getElementById("image_concerts").src = "images/rugby1.jpg";
+    document.getElementById("image_concerts").src = "https://lh3.googleusercontent.com/pw/AP1GczMumtq5GIUIbOjVYIShp7KaKtwwyi1S77tmBXwpdchgL4OulK5hYbTE00SZDxpTyaM0XZLbVck31V1otY5ZK1I8gLcxybMExw66n0yKLDk3TXu31ZNi4D4UldlDaOTvbZhcyA_XoHAjm_ij1GS-KtUT";
 }
 function rugby2() {
-    document.getElementById("image_concerts").src = "images/rugby2.png";
+    document.getElementById("image_concerts").src = "https://lh3.googleusercontent.com/pw/AP1GczOICpsrKLM0XN4A5VcgA56zSeURWAhGsMzT-pJ2BS1evRDCVFjjWDnok_1imlZcCjv8dyTV41te9VZZ6_KaQ1F4Vag8uZ2l27L_r8TB26rE1qdPHUj5Mex9AGSoMg2yUlybhdxRatiHNv3Udvph56bK";
 }
 function kinepolis() {
     document.getElementById("image_concerts").src = "images/zzzzzzzzzzzzzzzzzzzzzzzzzzzzzz";
 }
 function rue() {
-    document.getElementById("image_concerts").src = "images/rue.jpg";
+    document.getElementById("image_concerts").src = "https://lh3.googleusercontent.com/pw/AP1GczMMDnf-5LyRZVSogtCL2C2bLpGJEoklg6yqLwSOJR0OWhsL5ZA7nyJAKmgnA-xJTERVJiRgRgSpZSbprpsG8-WiKtPPwAPp_i2ctXEE7ExcZss2zRE1k99O5q0QCfmWdgMk4783N47Vt8moxoDpFjX5";
 }
